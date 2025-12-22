a70f9593916cecba88aefcbfa98cb4883d11f56a
Rafat Alam <126316989+rafat-alam@users.noreply.github.com>
2025-12-22 19:26:27 +0530

    Time: 5149 ms (5.28%), Space: 12.7 MB (64.9%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -4,30 +4,11 @@ class Solution(object):
         :type boxes: str
         :rtype: List[int]
         """
-        n = len(boxes)
         lis = [0] * len(boxes)
-        pf = [0] * (n + 1)
-        pfe = [0] * (n + 1)
-        suf = [0] * (n + 1)
-        sufe = [0] * (n + 1)
-
-        for i in range(0, n):
-            if boxes[i] == '1':
-                pf[i + 1] = pf[i] + pfe[i] + 1
-                pfe[i + 1] = pfe[i] + 1
-            else:
-                pf[i + 1] = pf[i] + pfe[i]
-                pfe[i + 1] = pfe[i]
-
-        for i in range(n - 1, -1, -1):
-            if boxes[i] == '1':
-                suf[i] = suf[i + 1] + sufe[i + 1] + 1
-                sufe[i] = sufe[i + 1] + 1
-            else:
-                suf[i] = suf[i + 1] + sufe[i + 1]
-                sufe[i] = sufe[i + 1]
-
-        for i in range(0, n):
-            lis[i] = pf[i] + suf[i + 1]
-
+        for i in range(0, len(boxes)):
+            for j in range(0, len(boxes)):
+                if i == j: continue
+                if boxes[j] == '1':
+                    lis[i] += abs(j - i)
+        
         return lis
